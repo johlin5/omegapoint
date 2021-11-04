cc12bfc0b302ba3740d45c1a6753b38b9acf49e5
Johan Lindholm <johan.lindholm@labtrino.com>
2021-11-04 13:46:09 +0100

    Refactor validty check for easier readability and slightly better performance

--- src/ValidityCheck.ts
+++ src/ValidityCheck.ts
@@ -1,35 +1,39 @@
 import { cleanIdInput, validateDate } from "./util";
 import {
-  personalIdRegexTest,
-  ordinationNumberRegexTest,
-  organisationNumberRegexTest,
+  isValidPersonalIdRegexTest,
+  isValidOrdinationNumberRegexTest,
+  isValidOrganisationNumberRegexTest,
 } from "./regex";
 import { luhnAlgorithm } from "./luhnAlgorithm";
 import readline from "readline";
 
 export const ValidityCheckForPersonalId = (id: string): boolean => {
-  const isValidPersonalId = personalIdRegexTest(id);
-  const isValidOrdinationNumber = ordinationNumberRegexTest(id);
-  const isValidOrganisationNumber = organisationNumberRegexTest(id);
-
-  switch (true) {
-    case isValidPersonalId:
-      if (!validateDate(id, false)) {
-        return false;
-      }
-      return luhnAlgorithm(cleanIdInput(id));
-
-    case isValidOrdinationNumber:
-      if (!validateDate(id, true)) {
-        return false;
-      }
-      return luhnAlgorithm(cleanIdInput(id));
-
-    case isValidOrganisationNumber:
-      return luhnAlgorithm(cleanIdInput(id));
-    default:
+  // 1: Check regex for personal ID
+  // 2: Validate correct date
+  // 3: Validate checksum with lunn algorithm
+  if (isValidPersonalIdRegexTest(id)) {
+    if (!validateDate(id, false)) {
       return false;
+    }
+    return luhnAlgorithm(cleanIdInput(id));
   }
+
+  // 1: Check regex for ordination ID
+  // 2: Validate correct date
+  // 3: Validate checksum with lunn algorithm
+  if (isValidOrdinationNumberRegexTest(id)) {
+    if (!validateDate(id, true)) {
+      return false;
+    }
+    return luhnAlgorithm(cleanIdInput(id));
+  }
+
+  // 1: Check regex for organization ID
+  // 2: Validate checksum with lunn algorithm
+  if (isValidOrganisationNumberRegexTest(id)) {
+    return luhnAlgorithm(cleanIdInput(id));
+  }
+  return false;
 };
 
 const readFromInput = () => {
--- src/regex.ts
+++ src/regex.ts
@@ -1,16 +1,16 @@
-export const personalIdRegexTest = (id: string): boolean => {
+export const isValidPersonalIdRegexTest = (id: string): boolean => {
   const validIdRegex = new RegExp(
     /^(18|19|20)?(\d{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[+-]?(\d{4}))$/
   );
   return validIdRegex.test(id);
 };
-export const ordinationNumberRegexTest = (id: string): boolean => {
+export const isValidOrdinationNumberRegexTest = (id: string): boolean => {
   const validIdRegex = new RegExp(
     /^(18|19|20)?(\d{2}(0[1-9]|1[0-2])(6[1-9]|7[0-9]|8[0-9]|9[0-1])[+-]?(\d{4}))$/
   );
   return validIdRegex.test(id);
 };
-export const organisationNumberRegexTest = (id: string): boolean => {
+export const isValidOrganisationNumberRegexTest = (id: string): boolean => {
   const validIdRegex = new RegExp(/^((16|18|19|20)?[2-9]\d{5}[-]\d{4})$/);
   return validIdRegex.test(id);
 };
